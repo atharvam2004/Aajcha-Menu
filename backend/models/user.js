97bc3f59bfc5392be1c785890a4db9ea4ca731f6
atharvam2004 <meheratharva49@gmail.com>
2024-10-10 02:31:59 +0530

    Reviewform

---
+++ backend/models/user.js
@@ -0,0 +1,15 @@
+// *****************************
+// @author - Atharva Meher  **
+// ****************************
+
+const mongoose = require("mongoose");
+
+const someModel = new mongoose.Schema({
+  name: String,
+  email: String,
+  password: String,
+});
+
+//Taking the model name user, which automatically creates a collection named as 'users'
+const user = mongoose.model("user", someModel);
+module.exports = user;
